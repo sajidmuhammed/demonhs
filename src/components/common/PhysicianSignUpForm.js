f9ee8fdf9b75220861d617ddf80a6e5c12fe947c
Muhammed Sajid MS <msajid@chisquarelabs.loc>
2025-07-10 16:00:53 +0530

    issue with import fixed for physician signup component

--- src/components/common/PhysicianSignUpForm.js
+++ src/components/common/PhysicianSignUpForm.js
@@ -14,6 +14,7 @@ import {
   descriptionValidation,
 } from "../../helpers/validation";
 import ErrorMessage from "../reusableFields/ErrorMessage";
+import useSignupForm from "../../hooks/useFormSignUp";
 
 const PhysicianSignupForm = () => {
 
